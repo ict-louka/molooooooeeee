6462b5428f8bfc3ff2b3e7ad5274a368b5b138c5
ict-louka <74817259+ict-louka@users.noreply.github.com>
2021-03-06 12:25:31 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,66 +1,46 @@
 input.onGesture(Gesture.Shake, function () {
+    const p = 100
     basic.showNumber(3)
-    basic.pause(500)
+    basic.pause(p)
     basic.showNumber(2)
-    basic.pause(500)
+    basic.pause(p)
     basic.showNumber(1)
-    basic.pause(500)
+    basic.pause(p)
     basic.showString("Go!")
     basic.pause(100)
-    mol = randint(1, 2)
-    
-
-    if (mol=2) {
-        basic.showLeds(`
-            # . . . .
-            # . . . .
-            # . . . .
-            . . . . .
-            # . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . #
-            . . . . #
-            . . . . #
-            . . . . .
-            . . . . #
-            `)
-    }
-    let knopingeduwd=0;
-    while (knopingeduwd=0) {
-    	if (input.buttonIsPressed(Button.A)) {knopingeduwd=1;}
-        
-        if (input.buttonIsPressed(Button.B)) {knopingeduwd=2;
+    let spelbezig=true
+    while (spelbezig==true){
+        let mol = Math.randomRange(1, 2)
+        if (mol==1) {
+            basic.showLeds(`
+                # . . . .
+                # . . . .
+                # . . . .
+                . . . . .
+                # . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . .
+                . . . . #
+                `)
         }
-    
-    }
-     if ((mol=1) && (knopingeduwd=1))
-     {
-
-        music.playTone(Note.C, music.beat())
-     }
-    if ((mol=2) && (knopingeduwd=2))
-     {
-
-        music.playTone(Note.C, music.beat())
-     }
-    
-    
-    
-    
-    
-    if (mol=1) {
-        input.buttonIsPressed(Button.A)
-    } else if (mol=2) {
-        input.buttonIsPressed(Button.B)
-    } else {
-    	
-    }
-    for (let index = 0; index < aantal; index++) {
-    	
-    }
+        let knopingeduwd=0;
+        while (knopingeduwd==0) {
+            if (input.buttonIsPressed(Button.A)) {knopingeduwd=1;}
+            if (input.buttonIsPressed(Button.B)) {knopingeduwd=2;
+            }
+        }
+        if (mol==knopingeduwd)      {
+            music.playTone(Note.A, music.beat())
+        }
+        else {
+            music.playTone(Note.C, music.beat())
+            spelbezig=false}	
+    }  
+    basic.showString("Game over!")
 })
-let aantal = 0
-let mol = 0
-aantal = 1
+
